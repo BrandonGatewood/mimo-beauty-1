d64d0511f4c063a43250c662601a03e64f5c09d1
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2023-10-02 16:52:05 -0700

    Added js to gather email information to send out

---
+++ js/email.js
@@ -0,0 +1,8 @@
+$(document).ready( function() {
+    $('#send').click(function() {
+        var name = $('name').val();
+        var email = $('#email').val();
+        var message = $('#message').val();
+
+    })
+});
